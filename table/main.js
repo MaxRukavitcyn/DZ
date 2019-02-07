85f40f6e23b6d4c8db32b2dd3bd73c991a67548c
MaxHand <rukav77@gmail.com>
2019-02-08 01:01:48 +0500

    filter

--- table/main.js
+++ table/main.js
@@ -2,67 +2,74 @@
  * Created by rukavitsyn-mn on 06.02.2019.
  */
 (function () {
-	function _tableManager(element, headers, data) {
-		let _data = data;
-		let keysOfHeaders = Object.keys(headers);
-		let table = _s('table').to(element);
-		let tr = _s('tr').to(_s('thead').to(table));
-		keysOfHeaders.forEach(h=>tr.do(_s('th').setText(headers[h])));
-		let tbody = _s('tbody').to(table);
-		
-		let columnSort, columnABC = true;
-		
-		function compare(a, b){
-			function toString(val){
-				if(!val) return '';
-				return val.toString();
-			}
-			return toString(a).localeCompare(toString(b))*(!columnABC?-1:1)
-		}
-		
-		this.update = function(){
-			tbody.removeChild();
-			let data = _data.slice();
-			if(columnSort) data.sort((a,b)=>compare(a[columnSort],b[columnSort]));
-			data.forEach(d=> {
-					let tr = _s('tr').to(tbody);
-					keysOfHeaders.forEach(h=>_s('td').to(tr).setText(d[h]))
-				}
-			);
-		};
-		
-		this.sort = function(columnKey){
-			if(keysOfHeaders.includes(columnKey)){
-				if(columnSort!==columnKey){
-					columnSort=columnKey;
-					columnABC = true
-				} else {
-					if(!columnABC) {
-						columnSort = null;
-					} else {
-						columnABC = !columnABC;
-					}
-				}
-				this.update();
-			}
-		}
-		
-		this.filter = function(phrase) {
-			if(phrase) {
-				
-				this.update();
-				
-			}
-			
-			
-		}
-		
-		this.update();
-	}
-	
-	window.tableManager = function (element, headers, data) {
-		return new _tableManager(element, headers, data)
-		
-	};
-	
+    function _tableManager(element, headers, data) {
+        let _data = data;
+        let searchPhrase = '';
+        let keysOfHeaders = Object.keys(headers);
+        let table = _s('table').to(element);
+        let tr = _s('tr').to(_s('thead').to(table));
+        keysOfHeaders.forEach(h => tr.do(_s('th').setText(headers[h])));
+        let tbody = _s('tbody').to(table);
+        let columnSort, columnABC = true;
+        function toString(val) {
+            if (!val) return '';
+            return val.toString();
+        }
+
+        function compare(a, b) {
+            return toString(a).localeCompare(toString(b)) * (!columnABC ? -1 : 1)
+        }
+
+        this.update = function () {
+            tbody.removeChild();
+            let data = _data.slice();
+            if (columnSort) data.sort((a, b) => compare(a[columnSort], b[columnSort]));
+            data.filter(d => {
+                if(searchPhrase !== '') {
+                    let isContains = false;
+                    keysOfHeaders.forEach(h => {
+                        if(toString(d[h]).indexOf(searchPhrase) !== -1){
+                            isContains = true;
+                        }
+                    });
+                    return isContains;
+                } else {
+                    return true;
+                }
+            }).forEach(d => {
+                    let tr = _s('tr').to(tbody);
+                    keysOfHeaders.forEach(h => _s('td').to(tr).setText(d[h]))
+                }
+            );
+        };
+
+        this.sort = function (columnKey) {
+            if (keysOfHeaders.includes(columnKey)) {
+                if (columnSort !== columnKey) {
+                    columnSort = columnKey;
+                    columnABC = true
+                } else {
+                    if (!columnABC) {
+                        columnSort = null;
+                    } else {
+                        columnABC = !columnABC;
+                    }
+                }
+                this.update();
+            }
+        };
+
+        this.filter = function (phrase) {
+            searchPhrase = toString(phrase);
+            this.update();
+        };
+
+        this.update();
+    }
+
+    window.tableManager = function (element, headers, data) {
+        return new _tableManager(element, headers, data)
+
+    };
+
 })();
--- table/test.js
+++ table/test.js
@@ -1,20 +1,16 @@
 "use strict";
-// let users = [
-// 	{firstName: 'Максим', lastName: 'Рукавицын', age: 28},
-// 	{firstName: 'Максим1', lastName: 'Рукавицын1', age: 29},
-// 	{firstName: 'Максим2', age: 30},
-// 	{firstName: 'Максим3', lastName: 'Рукавицын3', age: 31},
-// 	{firstName: 'Максим4', lastName: 'Рукавицын4', age: 34},
-// 	{lastName: 'Рукавицын5', firstName: 'Максим5', age: 54},
-// 	{firstName: 'Максим6', lastName: 'Рукавицын6'},
-// 	{firstName: 'Максим7', lastName: 'Рукавицын7', age: 88},
-// 	{firstName: 'Максим8', lastName: 'Рукавицын8', age: 76}
-// ];
-// let headersForTable = {firstName: 'имя', lastName: 'фамилия', age: 'возраст', kaka: 'rrrr'};
-//
-//
-// let table1 = new tableManager(document.body, headersForTable, users);
+let users = [
+	{firstName: 'Максим', lastName: 'Рукавицын', age: 28},
+	{firstName: 'Максим1', lastName: 'Рукавицын1', age: 29},
+	{firstName: 'Максим2', age: 30},
+	{firstName: 'Максим3', lastName: 'Рукавицын3', age: 31},
+	{firstName: 'Максим4', lastName: 'Рукавицын4', age: 34},
+	{lastName: 'Рукавицын5', firstName: 'Максим5', age: 54},
+	{firstName: 'Максим6', lastName: 'Рукавицын6'},
+	{firstName: 'Максим7', lastName: 'Рукавицын7', age: 88},
+	{firstName: 'Максим8', lastName: 'Рукавицын8', age: 76}
+];
+let headersForTable = {firstName: 'имя', lastName: 'фамилия', age: 'возраст', kaka: 'rrrr'};
 
-let sum = new Function('a,c', 'if(a<c) {return true} else {return false}');
 
-console.log(sum(4,5));
+let table1 = new tableManager(document.body, headersForTable, users);
